7b987c1abddc24c11c83f4cfbc402a725d5bc6ad
lfree <xh@ayjiayou.com>
2017-07-28 19:32:45 +0800

    gocd:agent-docker:promote privilege

--- GoCD/create-agent-docker.sh
+++ GoCD/create-agent-docker.sh
@@ -7,4 +7,5 @@ docker run \
     -e GO_SERVER_URL=https://172.17.0.3:8154/go \
     -e AGENT_AUTO_REGISTER_KEY="35d07d00-6ef0-459a-9ceb-3845a3039717" \
     -e AGENT_AUTO_REGISTER_HOSTNAME=agent-docker \
+    --privileged \
     ayjiayou/gocd-agent-docker
