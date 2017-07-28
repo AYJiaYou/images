3423365ac520da87ff3482be57af9746b10ed748
lfree <xh@ayjiayou.com>
2017-07-28 17:37:30 +0800

    gocd-agent-docker: fix create script error

--- GoCD/create-agent-docker.sh
+++ GoCD/create-agent-docker.sh
@@ -3,4 +3,7 @@
 docker run \
     -d \
     --name ay-gocd-agent3 \
+    -e GO_SERVER_URL=https://172.17.0.3:8154/go \
+    -e AGENT_AUTO_REGISTER_KEY="35d07d00-6ef0-459a-9ceb-3845a3039717" \
+    -e AGENT_AUTO_REGISTER_HOSTNAME=agent-docker \
     ayjiayou/gocd-agent-docker
