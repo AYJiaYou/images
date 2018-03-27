ea9b21ecee52fd3ada4489162763d20263b789c5
lfree <xh.dreamlover@gmail.com>
2018-03-27 18:46:05 +0700

    dokuwiki: support bind-mount

--- dokuwiki/README.md
+++ dokuwiki/README.md
@@ -1,27 +1,25 @@
 **This image is based on [istepanov/docker-dokuwiki](https://github.com/istepanov/docker-dokuwiki).**
 
-
 ## Simple Run
 `docker run -d -p 80:80 --name dokuwiki ayjiayou/dokuwiki`
 
-Access `http://localhost/install.php` then to setup the web.
-
+Access `http://localhost/install.php` to setup the web.
 
 ## Run With Data Persistant
 
-### Create A Data Container
+### Data Volume
+
+#### Create A Data Container
 `docker create -v /var/dokuwiki-storage --name dokudata ayjiayou/dokuwiki /bin/true`
 
-### Create The Executable Container With Volume Mounted From The Data Container
+#### Create The Executable Container With Volume Mounted From The Data Container
 `docker run -d -p 80:80 --volumes-from dokudata:z --name dokuwiki ayjiayou/dokuwiki`
 
-
-## Backup Contents Of `dokudata` Container To Host File
-
+#### Backup Contents Of `dokudata` Container To Host File
 `docker run --rm --volumes-from dokudata:z -v $(pwd):/backup ayjiayou/dokuwiki tar -cvf /backup/backup.tar /var/dokuwiki-storage`
 
-
-## Restore Contents Of `dokudata` Container From Host File
-
+#### Restore Contents Of `dokudata` Container From Host File
 'docker run --rm --volumes-from dokudata:z -v $(pwd):/backup  ayjiayou/dokuwiki bash -c "cd /var/dokuwiki-storage && tar -xvf /backup/backup.tar --strip 1"'
 
+### Bind Mount
+`docker run -d -p 80:80 -v /var/home/foo:/var/dokuwiki-storage:z --name dokuwiki ayjiayou/dokuwiki`
--- dokuwiki/start.sh
+++ dokuwiki/start.sh
@@ -2,6 +2,44 @@
 
 set -e
 
+# only run once
+if [[ -d /var/dokuwiki ]]; then
+    # change /var/dokuwiki to /var/www
+    mkdir -p /var/dokuwiki-storage/data /var/dokuwiki-storage/lib && \
+        rm -rf /var/www && \
+        mv /var/dokuwiki /var/www && \
+
+    # If there already have data in target folder, use it then. Else use default data.
+    if [[ ! -d /var/dokuwiki-storage/data/pages ]]; then
+        mv /var/www/data/pages /var/dokuwiki-storage/data/pages && \
+        mv /var/www/data/meta /var/dokuwiki-storage/data/meta && \
+        mv /var/www/data/media /var/dokuwiki-storage/data/media && \
+        mv /var/www/data/media_attic /var/dokuwiki-storage/data/media_attic && \
+        mv /var/www/data/media_meta /var/dokuwiki-storage/data/media_meta && \
+        mv /var/www/conf /var/dokuwiki-storage/conf && \
+        mv /var/www/data/attic /var/dokuwiki-storage/data/attic && \
+        mv /var/www/lib/plugins /var/dokuwiki-storage/lib/plugins
+    fi
+
+    # make link
+    rm -rf /var/www/data/media && \
+        ln -fs /var/dokuwiki-storage/data/media /var/www/data/media && \
+        rm -rf /var/www/data/pages && \
+        ln -fs /var/dokuwiki-storage/data/pages /var/www/data/pages && \
+        rm -rf /var/www/data/meta && \
+        ln -fs /var/dokuwiki-storage/data/meta /var/www/data/meta && \
+        rm -rf /var/www/data/media_attic && \
+        ln -fs /var/dokuwiki-storage/data/media_attic /var/www/data/media_attic && \
+        rm -rf /var/www/data/media_meta && \
+        ln -fs /var/dokuwiki-storage/data/media_meta /var/www/data/media_meta && \
+        rm -rf /var/www/conf && \
+        ln -fs /var/dokuwiki-storage/conf /var/www/conf && \
+        rm -rf /var/www/data/attic && \
+        ln -fs /var/dokuwiki-storage/data/attic /var/www/data/attic && \
+        rm -rf /var/www/lib/plugins && \
+        ln -fs /var/dokuwiki-storage/lib/plugins /var/www/lib/plugins
+fi
+
 chown -R nobody /var/www
 chown -R nobody /var/tmp
 chown -R nobody /var/dokuwiki-storage
